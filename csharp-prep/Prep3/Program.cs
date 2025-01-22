676251620ee680744c0360056d3fe8f3d30c1326
ncfurtado <fur22004@byui.edu>
2025-01-22 14:20:41 -0700

    Added the Resume assignment, and the prepare assignements.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        // string inputguess = "";
+        // Guess My Number game
+        int tallyGuesses = 0;
+        int guess = 0;
+
+        Console.WriteLine("Welcome to the Guess My Number game!");
+
+        Console.WriteLine("What is the magic number?: ");
+        string magicString = Console.ReadLine();
+        int magicNumber = int.Parse(magicString);
+        List<int> listOfNumbers = new List<int>();
+
+    
+        while (magicNumber != guess)
+        {
+            Console.WriteLine("What is your guess?: ");
+            string inputguess = Console.ReadLine();
+            guess = int.Parse(inputguess);
+
+            listOfNumbers.Add(guess);
+            tallyGuesses++;
+
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Too high!");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Too low!");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+        Console.WriteLine($"You guessed the magic number {magicNumber} in {tallyGuesses} guesses!");
+
     }
 }
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,32 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+        // Think of programming with classes as working with 'nouns.'
+        // If you've got a program, talk about things in basic terms and 
+        // then identify specifics like 'jobs' and 'resume'. Often these
+        // nouns will be used as classes, and methods programmed around them.
+        Job job1 = new Job();
+        job1._company = "Microsoft Corporation";
+        job1._jobTitle = "Intern";
+        job1._startYear = 2015;
+        job1._endYear = 2016;
+
+        job1.Display();
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Developer";
+        job2._startYear = 2016;
+        job2._endYear = 2017;
+
+
+        Resume myResume = new Resume();
+        myResume._name = "Daniel Game";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+        
+
     }
 }
